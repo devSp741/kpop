bbccfe331eacb036eb63bdb514909c811971a953
gbyte-dev <gbytedeveloper@gmail.com>
2026-09-10 11:23:31 +0530

    cors setup

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  allowedDevOrigins: ['127.0.0.1', 'localhost', '192.168.1.51'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
